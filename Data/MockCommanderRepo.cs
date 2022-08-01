ccd5d09c509d0bde31d8e794007f795be7a7fedd
ouko <oukoezzy@gmail.com>
2022-08-01 20:19:47 +0300

    add repo

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,47 @@
+﻿using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line="Boil water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "cut bread", Line="Get a knife", Platform = "Knife & chopping board" },
+                new Command { Id = 2, HowTo = "make cup of tea", Line="Boil water", Platform = "Kettle & cup" },
+
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line="Boil water", Platform = "Kettle & Pan" };
+            
+
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Commander.Data;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+    (builder.Configuration.GetConnectionString("CommanderConnection")));
+builder.Services.AddControllers().AddNewtonsoftJson(s =>
+{
+    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();   
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
